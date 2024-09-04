705cea1f5fab638eccd98fa07e9ac17847e289e8
NtwariMikeCK <k.ntwari@alustudent.com>
2024-09-04 14:26:49 -0700

    Working hard always pays off

---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Check if exactly one argument is passed
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Get the input string
+input = ARGV[0]
+
+# Use a regular expression to find all occurrences of 'School'
+matches = input.scan(/hbt{2,5}n/)
+
+# Print the matches joined together
+puts matches.join

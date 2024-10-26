656690b4e9183ad1673c6f414d14aa2ecdc5d8c1
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2024-10-26 13:28:14 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches the titles of 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints 'None'.
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        # Get the first 10 posts
+        posts = json_data.get('data', {}).get('children', [])
+        if posts:
+            for i in range(min(10, len(posts))):  # Print available titles, up to 10
+                print(posts[i].get('data', {}).get('title'))
+            print("OK")  # Print "OK" after successfully displaying titles
+        else:
+            print("None")
     else:
         print("None")
